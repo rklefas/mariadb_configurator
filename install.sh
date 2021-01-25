1045e808c6f393a554075d757d378d5c07254598
Ryan Klefas <ryan.klefas@gmail.com>
2021-01-25 18:53:07 +0000

    Add scripts for showing mariadb version and replication settings.

---
+++ install.sh
@@ -0,0 +1,5 @@
+apt install python3-pip
+apt install libmariadb-dev-compat
+pip3 install mariadb
+pip3 install tabulate
+
---
+++ show_version.py
@@ -0,0 +1,31 @@
+# Module Imports
+import mariadb
+import sys
+from tabulate import tabulate
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user=config['LOGIN']['user'],
+        password=config['LOGIN']['password'],
+        host=config['LOGIN']['host'],
+        port=3306,
+        database="mysql"
+
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
+
+cur.execute("SHOW VARIABLES LIKE '%version%'")
+myresult = cur.fetchall()
+
+print(tabulate(myresult, headers=['Name', 'Value'], tablefmt='psql'))
