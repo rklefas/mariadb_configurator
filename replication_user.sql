e2f20e0a5a882b2a1fdb6745511a72adf031b53a
Ryan Klefas <ryan.klefas@gmail.com>
2021-02-09 20:04:31 +0000

    Test the connections in show_version.py and prompt the user on how to correctly configure mariadb.

---
+++ replication_user.sql
@@ -0,0 +1,2 @@
+create user root@'%' identified by 'root';
+
--- show_version.py
+++ show_version.py
@@ -7,17 +7,34 @@ import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
 
+server = input("Read (M)ASTER or (S)LAVE server? ")
+server = server.upper()
+
+if server == "M":
+    server = "MASTER"
+elif server == "S":
+    server = "SLAVE"
+
 
 # Connect to MariaDB Platform
 try:
     conn = mariadb.connect(
-        user=config['LOGIN']['user'],
-        password=config['LOGIN']['password'],
-        host=config['LOGIN']['host'],
-        port=3306
+        user=config[server]['user'],
+        password=config[server]['password'],
+        host=config[server]['host'],
+        port=int(config[server]['port'])
     )
 except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
+    print(f"{e}")
+    print("")
+
+    if str(e).startswith("Can't connect to MySQL server"):
+        print("- Open config file, probably at:  /etc/mysql/mariadb.conf.d/50-server.cnf")
+        print("- Ensure the server is accessiible with the bind-address config option.")
+        print("- Restart server:  /etc/init.d/mysql restart")
+    elif str(e).startswith("Access denied for user"):
+        print("1) Ensure the connection credentials in config.ini are correct. ")
+    print("")
     sys.exit(1)
 
 # Get Cursor
