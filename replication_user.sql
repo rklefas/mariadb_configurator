7c3d9d60f4911ebe0c0b7680933ad6c968323a4f
Ryan Klefas <ryan.klefas@gmail.com>
2021-02-10 21:28:00 -0500

    Change user account.

--- replication_user.sql
+++ replication_user.sql
@@ -1,3 +1,3 @@
-create user root@'%' identified by 'root';
-GRANT SUPER, REPLICATION CLIENT on *.* to root@'%';
+create user replication_user@'%' identified by 'replication_user';
+GRANT SUPER, REPLICATION CLIENT on *.* to replication_user@'%';
 
